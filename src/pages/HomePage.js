a92106583d48182190542e820716c7a442b5ef32
Gabe S <gabesliwowicz@gmail.com>
2023-05-26 16:49:54 -0400

    Added a homepage image, decided not to use.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import "../css/HomePage.css";
 import Header from "../components/Header";
 import Cart from "../components/Cart";
 
@@ -17,7 +18,10 @@ const HomePage = () => {
             ) : (
                 <Cart onCloseCart={handleToggleCart} />
             )}
-            <h1>Home</h1>
+            <h1>Office Supplies Shopping Cart Application Demo</h1>
+            <div className="home-page">
+                {/* <p>Image by <a href="https://www.freepik.com/free-photo/notebook-felt-tip-pen-eyeglasses-stationery-cellphone-white-background_2905880.htm#query=office%20supplies%20background&position=24&from_view=keyword&track=ais">Freepik</a></p> */}
+            </div>
         </div>   
     )
 }
